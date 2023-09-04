bd2001b132b94909fe21cdb6d5ce9e56fa810e3c
Thai Le <lqthai693@gmail.com>
2023-09-04 23:42:54 +0700

    Init basic template

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import PatientPage from "./pages/PatientPage";
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import "primereact/resources/primereact.min.css";
-import DefaultLayout from "./components/DefaultLayout";
-import Form from "./components/Form";
+import PropTypes from 'prop-types';
+import React from 'react';
 
-function App() {
+import { Switch, BrowserRouter as Router } from 'react-router-dom';
+
+// Import Routes all
+import { userRoutes, authRoutes } from './routes/allRoutes';
+
+// Import all middleware
+import Authmiddleware from './routes/middleware/Authmiddleware';
+
+// layouts Format
+import VerticalLayout from './components/VerticalLayout/';
+import NonAuthLayout from './components/NonAuthLayout';
+
+// Import scss
+import './assets/scss/theme.scss';
+import './assets/scss/preloader.scss';
+
+// import fakeBackend from './helpers/AuthType/fakeBackend';
+// // Activating fake backend
+// fakeBackend();
+
+const App = () => {
+  const Layout = VerticalLayout;
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/patient" element={<PatientPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          {authRoutes.map((route, idx) => (
+            <Authmiddleware
+              path={route.path}
+              layout={NonAuthLayout}
+              component={route.component}
+              key={idx}
+              isAuthProtected={false}
+              exact
+            />
+          ))}
+
+          {userRoutes.map((route, idx) => (
+            <Authmiddleware
+              path={route.path}
+              layout={Layout}
+              component={route.component}
+              key={idx}
+              isAuthProtected={false}
+              exact
+            />
+          ))}
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
-}
+};
+
+App.propTypes = {
+  layout: PropTypes.any
+};
 
 export default App;
