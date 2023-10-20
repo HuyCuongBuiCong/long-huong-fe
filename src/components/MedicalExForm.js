56fdd77d4f4861b3404397e70ae481e941f6a8b8
nqminh <mrakira1991@gmail.com>
2023-10-21 02:15:53 +0700

    update code style

--- src/components/MedicalExForm.js
+++ src/components/MedicalExForm.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { InputTextarea } from "primereact/inputtextarea";
-import { Button } from "primereact/button";
-import { InputText } from "primereact/inputtext";
-import { AutoComplete } from "primereact/autocomplete";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
-import beAxios from "../config.js";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { AutoComplete } from 'primereact/autocomplete';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import beAxios from '../config.js';
 
 const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
   const navigate = useNavigate();
   const [selectedDiseases, setSelectedDiseases] = useState([]);
   const [diseaseSuggestions, setDiseaseSuggestions] = useState([]);
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
   const [prescriptionIds, setPrescriptionIds] = useState([]);
   const [prescriptionSuggestions, setPrescriptionSuggestions] = useState([]);
 
   const [diseases, setDiseases] = useState([]);
   const [prescriptions, setPrescriptions] = useState([]);
 
-  const [description, setDescription] = useState("");
+  const [description, setDescription] = useState('');
   const [diseaseCode, setDiseaseCode] = useState([]);
   const [diseaseNames, setDiseaseNames] = useState([]);
   const [formMedical, setFormMedical] = useState(true);
@@ -37,7 +37,7 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
   };
 
   const handleDiseaseNameChange = (e) => {
-    setDiseaseNames(e.target.value.split(","));
+    setDiseaseNames(e.target.value.split(','));
   };
 
   const search = (event) => {
@@ -53,14 +53,12 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
     if (selectedPrescription) {
       const selectedPrescriptionId = selectedPrescription.id;
       setPrescriptionIds((prevIds) => [...prevIds, selectedPrescriptionId]);
-      setValue("");
+      setValue('');
       onPrescriptionChange([...prescriptionIds, selectedPrescriptionId]);
     }
   };
   const handleRemove = (prescriptionId) => {
-    const updatedPrescriptions = prescriptionIds.filter(
-      (item) => item !== prescriptionId
-    );
+    const updatedPrescriptions = prescriptionIds.filter((item) => item !== prescriptionId);
     setPrescriptionIds(updatedPrescriptions);
     onPrescriptionChange(updatedPrescriptions);
   };
@@ -68,9 +66,7 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
   const searchDiseases = (event) => {
     const query = event.query;
     const filteredSuggestions = diseases.filter((disease) => {
-      return (
-        disease.diseaseCode.toLowerCase().indexOf(query.toLowerCase()) !== -1
-      );
+      return disease.diseaseCode.toLowerCase().indexOf(query.toLowerCase()) !== -1;
     });
     setDiseaseSuggestions(filteredSuggestions);
   };
@@ -88,9 +84,7 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
     );
     setSelectedDiseases(updatedSelectedDiseases);
 
-    const updatedDiseaseNames = diseaseNames.filter(
-      (name) => name !== unselectedDisease.name
-    );
+    const updatedDiseaseNames = diseaseNames.filter((name) => name !== unselectedDisease.name);
     setDiseaseNames(updatedDiseaseNames);
   };
 
@@ -103,7 +97,7 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
 
     const diseasesToSend = selectedDiseases.map((disease) => ({
       diseaseCode: disease.diseaseCode,
-      name: disease.name,
+      name: disease.name
     }));
 
     const prescriptionIdsArray = prescriptionIds;
@@ -111,24 +105,21 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
       diseases: diseasesToSend,
       prescriptionIds: prescriptionIdsArray,
       description: description,
-      patient_id: selectedPatientId,
+      patient_id: selectedPatientId
     };
 
     try {
-      const res = await beAxios.post(
-        `/medical-records/${selectedPatientId}`,
-        data
-      );
+      const res = await beAxios.post(`/medical-records/${selectedPatientId}`, data);
       if (res && res.data) {
-        toast.success("Đã tạo phiếu khám bệnh thành công");
-        navigate("/patient_detail");
+        toast.success('Đã tạo phiếu khám bệnh thành công');
+        navigate('/patient_detail');
         setFormMedical(false);
       } else if (res && res.data.message) {
         toast.error(res.data.message);
       }
     } catch (error) {
       console.log(error);
-      toast.error("Có lỗi xảy ra khi tạo phiếu bệnh nhân");
+      toast.error('Có lỗi xảy ra khi tạo phiếu bệnh nhân');
     }
   };
 
@@ -142,7 +133,7 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
         }
       })
       .catch((error) => {
-        console.error("Lỗi khi tải dữ liệu từ backend:", error);
+        console.error('Lỗi khi tải dữ liệu từ backend:', error);
       });
   }, []);
   return (
@@ -155,10 +146,7 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
                 <label>Mã bệnh</label>
                 <br />
                 <span className="p-input-icon-left">
-                  <i
-                    className="pi pi-qrcode"
-                    style={{ color: "var(--primary-color)" }}
-                  />
+                  <i className="pi pi-qrcode" style={{ color: 'var(--primary-color)' }} />
                   <AutoComplete
                     placeholder="Mã bệnh"
                     value={selectedDiseases}
@@ -176,14 +164,11 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
                 <label>Tên bệnh</label>
                 <br />
                 <span className="p-input-icon-left">
-                  <i
-                    className="pi pi-th-large"
-                    style={{ color: "var(--primary-color)" }}
-                  />
+                  <i className="pi pi-th-large" style={{ color: 'var(--primary-color)' }} />
                   <InputText
                     type="text"
                     placeholder="Nhập tên bệnh"
-                    value={diseaseNames.join(", ")}
+                    value={diseaseNames.join(', ')}
                     onChange={handleDiseaseNameChange}
                     style={{ width: 255 }}
                   />
@@ -209,14 +194,8 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
                   <ul>
                     {prescriptionIds.map((prescriptionId) => (
                       <li key={prescriptionId} className="me-2">
-                        {
-                          prescriptions.find((p) => p.id === prescriptionId)
-                            .name
-                        }{" "}
-                        <i
-                          className="pi pi-times"
-                          onClick={() => handleRemove(prescriptionId)}
-                        />
+                        {prescriptions.find((p) => p.id === prescriptionId).name}{' '}
+                        <i className="pi pi-times" onClick={() => handleRemove(prescriptionId)} />
                       </li>
                     ))}
                   </ul>
@@ -225,22 +204,12 @@ const MedicalExForm = ({ selectedPatientId, onPrescriptionChange }) => {
               <div className="input-field">
                 <label>Chuẩn đoán</label>
                 <br />
-                <InputTextarea
-                  placeholder="Chuẩn đoán"
-                  rows={5}
-                  cols={30}
-                  onChange={handleDescriptionChange}
-                />
+                <InputTextarea placeholder="Chuẩn đoán" rows={5} cols={30} onChange={handleDescriptionChange} />
               </div>
             </div>
           </div>
           <div className="form-center">
-            <Button
-              className="button"
-              label="Lưu"
-              type="submit"
-              icon="pi pi-check"
-            />
+            <Button className="button" label="Lưu" type="submit" icon="pi pi-check" />
           </div>
         </form>
       )}
