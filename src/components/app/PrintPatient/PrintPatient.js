e40db33ed9b3d26c588063a6a4b5892689eab087
nqminh <mrakira1991@gmail.com>
2023-10-21 22:46:40 +0700

    update print template

--- src/components/app/PrintPatient/PrintPatient.js
+++ src/components/app/PrintPatient/PrintPatient.js
@@ -86,140 +86,17 @@ const PrintPatient = ({ selectedMedical, patient }) => {
                 <th scope="col">#</th>
                 <th scope="col">Tên bài thuốc YHCT</th>
                 <th scope="col">Số lượng (thang)</th>
-                <th scope="col">STT</th>
-                <th scope="col">Tên vị thuốc YHCT</th>
-                <th scope="col">Số lượng</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Toa thuốc trị phổi</td>
-                <td></td>
-                <td>1</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Toa thuốc trị phổi có dịch</td>
-                <td></td>
-                <td>2</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Toa thuốc trị lao phổi</td>
-                <td></td>
-                <td>3</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">4</th>
-                <td>Toa thuốc gan</td>
-                <td></td>
-                <td>4</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">5</th>
-                <td>Toa thuốc thận số</td>
-                <td></td>
-                <td>5</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">6</th>
-                <td>Toa thuốc bồi bổ khí huyết</td>
-                <td></td>
-                <td>6</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">7</th>
-                <td>Toa thuốc thoái hóa-gai-viêm khớp (1)</td>
-                <td></td>
-                <td>7</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">8</th>
-                <td>Toa thuốc trị Parkinson</td>
-                <td></td>
-                <td>8</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">9</th>
-                <td>Toa thuốc trị tiểu đường</td>
-                <td></td>
-                <td>9</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">10</th>
-                <td>Toa thuốc trị mất ngủ, suy nhược thần kinh</td>
-                <td></td>
-                <td>10</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">11</th>
-                <td>Toa thuốc trị hen suyển</td>
-                <td></td>
-                <td>11</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">12</th>
-                <td>Toa thuốc trị vẩy nến</td>
-                <td></td>
-                <td>12</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">13</th>
-                <td>Toa thuốc trị mắt mờ đục thủy tinh thể</td>
-                <td></td>
-                <td>13</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">14</th>
-                <td>Toa thuốc trị phong ngứa - vẩy nến</td>
-                <td></td>
-                <td>14</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">15</th>
-                <td>Toa thuốc trị Tim</td>
-                <td></td>
-                <td>15</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <th scope="row">16</th>
-                <td>Toa thuốc trị tan máu bầm trong não - động kinh</td>
-                <td></td>
-                <td>16</td>
-                <td></td>
-                <td></td>
-              </tr>
+              {selectedMedical.medicalRecordPrescriptions &&
+                selectedMedical.medicalRecordPrescriptions.map((medicalRecordPrescription, index) => (
+                  <tr>
+                    <th scope="row">{index + 1}</th>
+                    <td>{medicalRecordPrescription.prescription.name}</td>
+                    <td>{medicalRecordPrescription.amount}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
